5b33707981b9fb9418382dd637bdb05ed374006a
Bennie Robinson <brobinson88@gmail.com>
2020-12-15 08:33:33 -0500

    Remove unused import from UsersList

--- client/src/features/users/UsersList.tsx
+++ client/src/features/users/UsersList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, ReactChildren } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { EntityId } from '@reduxjs/toolkit';
 
